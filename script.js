6bc19742efe2b72fb1c47d14841ef0df10b57028
Pablo Duenas <pabloduenas64@gmail.com>
2023-06-11 19:05:47 -0700

    project card redesign

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ $(document).ready(function () {
     $('.buttons button').click(function () {
       // Remove active class from all buttons
       $('.buttons button').removeClass('is-selected');
+      $('.buttons button').removeClass('is-link');
       // Add active class to the clicked button
       $(this).addClass('is-selected');
+      $(this).addClass('is-link');
   
       // Get the selected filter
       var filter = $(this).data('filter');
