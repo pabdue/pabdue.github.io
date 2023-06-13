96148a32830b0c5fbba8af6219b4792b49b435d3
Pablo Duenas <pabloduenas64@gmail.com>
2023-06-12 17:28:36 -0700

    Fix mobile view and styling, fix navbar menu, add comments.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/*
+    File: script.js
+    Description: This JavaScript file contains functions for filtering and displaying project cards on the projects.html page.
+    Author: Pablo Duenas
+*/
+
 $(document).ready(function () {
     // Filter button click event
     $('.buttons button').click(function () {
@@ -19,29 +25,5 @@ $(document).ready(function () {
         $('#project-cards .column[data-labels*="' + filter + '"]').show();
       }
     });
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
 });
   
